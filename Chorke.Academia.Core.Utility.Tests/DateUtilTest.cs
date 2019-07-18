168b24865331a030e2648dc7a90dfe9bb34f2409
Chorke Academia, Inc <devs@chorke.org>
2019-07-19 07:06:17 +0800

    ISO Date Format added

--- Chorke.Academia.Core.Utility.Tests/DateUtilTest.cs
+++ Chorke.Academia.Core.Utility.Tests/DateUtilTest.cs
@@ -12,6 +12,10 @@ namespace Chorke.Academia.Core.Utility.Tests
             DateTime date = new DateTime(1983, 10, 10);
             String actual = DateUtil.Format(date, DateUtil.DD_MM_YYYY);
             Assert.Equal("10/10/1983", actual);
+
+            date = new DateTime(1983, 10, 10, 10, 10, 10, 10);
+            actual = DateUtil.Format(date, DateUtil.ISO_DATE);
+            Assert.Equal("1983-10-10T10:10:10.010", actual);
         }
     }
 }
--- Chorke.Academia.Core.Utility/DateUtil.cs
+++ Chorke.Academia.Core.Utility/DateUtil.cs
@@ -6,11 +6,15 @@ namespace Chorke.Academia.Core.Utility
     public class DateUtil
     {
         public const string DD_MM_YYYY = "dd/MM/yyyy";
+        public const string ISO_DATE = "yyyy-MM-dd'T'HH:mm:ss.fff";
+        public const string ISO_8601 = "yyyy-MM-dd'T'HH:mm:ss.fffK";
 
         public static string Format(DateTime date) => Format(date, DD_MM_YYYY);
-        public static string Format(DateTime date, string format) => date.ToString(DD_MM_YYYY);
+        public static string FormatISO(DateTime date) => Format(date, ISO_DATE);
+        public static string Format(DateTime date, string format) => date.ToString(format);
 
         public static DateTime Parse(String date) => Parse(date, DD_MM_YYYY);
+        public static DateTime ParseISO(String date) => Parse(date, ISO_DATE);
         public static DateTime Parse(String date, string format) => DateTime.ParseExact(date, format, CultureInfo.InvariantCulture);
     }
 }
